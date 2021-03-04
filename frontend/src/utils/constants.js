246f36ee3b08824c51d31c83d00eb9105449a2b0
Kulakov Pavel <kombojiec@gmail.com>
2021-03-04 23:26:06 +0300

    master: delete .env file && fix pathes

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,6 +1,6 @@
 
 const ownerInfo = {
-  BASE_URL: process.env.REACT_APP_BASE_URL || 'http://localhost:3000',  
+  BASE_URL: 'https://api.kombojiec.students.nomoreparties.space' || 'http://localhost:3000',  
 };
 
 export default ownerInfo;
